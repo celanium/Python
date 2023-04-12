b4c70f8f8b992e70feccf20fa81dd7f27203f995
Natali <celaniumzima@mail.ru>
2023-04-12 22:42:22 +0300

    Homework 5. prepare 1-7

---
+++ Lesson_5/5_2.py
@@ -0,0 +1,23 @@
+'''2. Создать текстовый файл (не программно), сохранить в нем несколько строк, выполнить подсчет количества строк,
+количества слов в каждой строке. Алгоритм: Слова в строке разделены пробелами, но не обязательно одним пробелом.'''
+
+def generator_file():
+    f_file_open = open('2.txt', 'w')
+    data_list = [
+                'This', 'method', 'is', 'supplied', 'with', 'the', 'MersenneTwister', 'generator',
+                'and', 'some', 'other', 'generators', 'may', 'also', 'provide',
+                'it', 'as', 'an', 'optional', 'part', 'of', 'the', 'API2'
+                ]
+    data = [
+        f_file_open.write(str(data_list[i]) + "\n") for i in range(len(data_list))
+    ]
+    f_file_open.close()
+generator_file()
+
+def count():
+    file_open = open('2.txt', 'r')
+    print("Count strings: " + str(len(file_open.readlines())) + ".")
+    file_open = open('2.txt', 'r')
+    print("Count elements: " + str(len(file_open.read())) + ".")
+
+count()
