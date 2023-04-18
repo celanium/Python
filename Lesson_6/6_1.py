73c4cbc508d7bc33eb933a4905e7a6793f4597f0
Natali <celaniumzima@mail.ru>
2023-04-18 23:42:59 +0300

    Homework 6. tasks 1-3

--- Lesson_6/6_1.py
+++ Lesson_6/6_1.py
@@ -7,94 +7,30 @@
 Алгоритм:
 - Подумайте должен ли атрибут color быть атрибутом класса или атрибутом экземпляра.
 - Используйте функцию sleep пакета time для формирования задержки
-- ВЫ можете использовать итератор по цветам или метод running может принимать аргумент - цвет, на который надо переключиться, тогда задачу можно усложнить, реализовав проверку порядка режимов, и при его нарушении выводить соответствующее сообщение и завершать скрипт.
+- ВЫ можете использовать итератор по цветам или метод running может принимать
+аргумент - цвет, на который надо переключиться, тогда задачу можно усложнить,
+реализовав проверку порядка режимов, и при его нарушении выводить соответствующее сообщение и завершать скрипт.
 - Попробуйте связать в одну структуру данных цвета и время задержек'''
 
-#вариант1
 
 from time import sleep
-from datetime import datetime as dt
 
 
 class TrafficLight:
-    """ Класс светофора, реализующий свое переключение при запуске running( """
-    _states = {'red': 7, 'yellow': 2, 'green': 10}
-    _color = ''
+    """ Класс светофора, реализующий переключение цветов при запуске run """
+    _indications = {'красный': 7, 'желтый': 2, 'зеленый': 8}
+    # Текущий цвет
+    _current_color = None
 
-    def running(self):
-        """ Метод запусключения светофора """
-        for color, sw_time in self._states.items():
-            self._color = color
-            start_state_time = dt.now()
-
-            print(f"TrafficLight switched to state '{self._color}' "
-                  f"on {sw_time} seconds")
-
-            sleep(sw_time)
-
-            print(f"TrafficLight leave state '{self._color}' after" 
-                  f"{(dt.now() - start_state_time).seconds} seconds")
+    def run(self):
+        """ Метод запуска цикла работы светофора """
+        for color, color_time in self._indications.items():
+            self._current_color = color
+            print(f"Светофор переключился на цвет '{self._current_color}' "
+                  f"на {color_time} сек")
+            sleep(color_time)
 
 
 if __name__ == '__main__':
     tl = TrafficLight()
-    tl.running()
-
-#вариант 2
-
-from time import sleep
-class TrafficLight:
-    # 7 + 2 + 6
-    try:
-        i_rate = int(input("How many reps? (One cycle - 15 sec. Cycle can't be more than 30 ): ->>> "))
-        if i_rate > 30:
-            raise BaseException
-    except BaseException:
-        print("Error. Please, enter to int number.")
-        exit(-1)
-
-    __color = ('red', 'yellow', 'green')
-
-    def running(self):
-        i_cycle = 0
-
-        while i_cycle < self.i_rate:
-            i_cycle += 1
-            i_text = "The traffic light is now on:"
-
-            for i_color in TrafficLight.__color:
-                if i_color == 'red':
-                    print(i_text, i_color)
-                    sleep(7)
-                elif i_color == 'yellow':
-                    print(i_text, i_color)
-                    sleep(2)
-                elif i_color == 'green':
-                    print(i_text, i_color)
-                    sleep(6)
-                else:
-                    print("Error. Please restart program.")
-                    exit(-1)
-
-TrafficLight().running()
-
-#вариант 3
-
-import time
-class TrafficLight:
-    _color = None
-    _colors = ['red', 'yellow', 'green']
-
-    def __init__(self):
-        self._color = self._colors[0]
-
-    def running(self):
-        i=0
-        while i<5:
-            for el in TrafficLight._colors :
-                print(el)
-                i+=1
-                time.sleep(1)
-
-traffic = TrafficLight()
-traffic.running()
+    tl.run()
--- Lesson_6/6_3.py
+++ Lesson_6/6_3.py
@@ -5,7 +5,6 @@
 Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные, проверить значения атрибутов,
 вызвать методы экземпляров).'''
 
-#вариант 1
 
 class Worker:
     """Класс работника"""
@@ -51,98 +50,10 @@ class Position(Worker):
 
 
 if __name__ == '__main__':
-    position_data = [
-        {
-            'name': 'Ivan',
-            'surname': 'Ivanov',
-            'position': 'Scrum master',
-            'wage':  40000,
-            'bonus': 0
-        },
-        {
-            'name': 'Petr',
-            'surname': 'Petrov',
-            'position': 'developer',
-            'wage':  90000,
-            'bonus': 60000
-        },
-        {
-            'name': 'Irina',
-            'surname': 'Ivanova',
-            'position': 'service delivery manager',
-            'wage':  60000,
-            'bonus': 30000
-        },
-    ]
-
-    for item in position_data:
-        position = Position(**item)
-
-        print('#######################################')
-        print('From data: ', item)
-        print('Position name: ', position.name)
-        print('Position surname: ', position.surname)
-        print('Position full name: ', position.get_full_name())
-        print('Position full name reverse: ', position.get_full_name(reverse=True))
-        print('Position position: ', position.position)
-        print('Position total income: ', position.get_total_income())
-        print('#######################################\n\n')
-
-#вариант 2
-
-class Worker:
-
-    def __init__(self, name, surname, position, i_wage, i_bonus):
-        self.name = name
-        self.surname = surname
-        self.position = position
-        self._income = {"wage": i_wage, "bonus": i_bonus}
-
-
-class Position(Worker):
-
-    def __init__(name, surname, position, i_wage, i_bonus):
-        super().__init__(name, surname, position, i_wage, i_bonus)
-
-    def get_full_name(self):
-        return self.name + ' ' + self.surname
-
-    def get_total_income(self):
-        return self._income.get('wage') + self._income.get('bonus')
-
-
-i_position = Position('Basil', 'Basil', 'manager', 5000, 10000)
-print(i_position.get_full_name())
-print(i_position.get_total_income())
-print(i_position.name)
-print(i_position.surname)
-print(i_position.position)
-print(i_position._income)
-
-#вариант 3
-
-class Worker:
-    name = None
-    surname = None
-    position = None
-    profit = None
-    bonus = None
-
-    def __init__(self, name, surname, position, profit, bonus):
-        self.name = name
-        self.surname = surname
-        self.position = position
-        self.profit = profit
-        self.bonus = bonus
-
-class Position(Worker):
-    def __init__(self, name, surname, position, profit, bonus):
-        super().__init__(name, surname, position, profit, bonus)
-    def get_full_name(self):
-        return self.name + self.surname
-    def get_full_profit(self):
-        self.__income = {'profit': self.profit, 'bonus': self.bonus}
-        return self.__income
-
-manager = Position('Petr', 'Petrov', 'manager', 500, 100)
-print(manager.get_full_name(), manager.get_full_profit())
+    position = Position('Иван', 'Петров', 'Инженер-программист', 50000, 10000)
+    print('Имя: ', position.name)
+    print('Фамилия: ', position.surname)
+    print('Полное имя: ', position.get_full_name())
+    print('Полное имя перевернутое: ', position.get_full_name(reverse=True))
+    print('Должность: ', position.position)
+    print('Общий доход: ', position.get_total_income())
