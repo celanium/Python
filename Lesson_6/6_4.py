6fdd14bad52a2ffad1d60ee15c26d3c245f3b1a2
Natali <celaniumzima@mail.ru>
2023-04-19 22:49:49 +0300

    Homework 6. tasks 1-5

--- Lesson_6/6_4.py
+++ Lesson_6/6_4.py
@@ -1,7 +1,11 @@
-'''4. Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты: speed, color, name, is_police (булево).
-А также методы: go, stop, turn(direction), которые должны сообщать, что машина поехала, остановилась, повернула (куда).
-Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar. Добавьте в базовый класс метод show_speed,
-который должен показывать текущую скорость автомобиля. Для классов TownCar и WorkCar переопределите метод show_speed.
+'''4. Реализуйте базовый класс Car. У данного класса должны быть следующие
+атрибуты: speed, color, name, is_police (булево).
+А также методы: go, stop, turn(direction), которые должны сообщать, что машина
+поехала, остановилась, повернула (куда).
+Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar.
+Добавьте в базовый класс метод show_speed,
+который должен показывать текущую скорость автомобиля. Для классов TownCar и
+WorkCar переопределите метод show_speed.
 При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
 Алгоритм:
 - метод turn(direction) должен иметь аргумент - направление поворота.
@@ -10,200 +14,93 @@
 Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.'''
 
-#вариант 1
 
-class Cars:
-    name = None
-    speed = None
-    color = None
-    is_police = False
-
-    def __init__(self, name, speed, color, is_police = False):
-        self.name = name
-        self.speed = speed
-        self.color = color
-        self.is_police = is_police
-    def go(self):
-        return "The car went"
-    def stop(self):
-        return "The car has stopped"
-    def turn(self, direction):
-        return "The car turned to " + direction
-
-class TownCar(Cars):
-    family = None
-    def __init__(self, name, speed, color, family = True):
-        super().__init__(name, speed, color)
-        self.family = family
-
-class SportCar(Cars):
-    def __init__(self, name, speed, color):
-        super().__init__(name, speed, color)
-
-class WorkCar(Cars):
-    def __init__(self, name, speed, color, is_police):
-        super().__init__(name, speed, color, is_police)
-
-class PoliceCar(Cars):
-    def __init__(self, name, speed, color):
-        super().__init__(name, speed, color, True)
-
-ford = TownCar('Ford', 60, 'black')
-print(ford.name, ford.color, ford.speed, ford.is_police)
-print(ford.go(), ford.turn('City'), ford.stop())
-sport = SportCar('Ford', 180, 'red')
-work1 = WorkCar('Ford', 90, 'white', True)
-work2 = WorkCar('Audi', 90, 'white', False)
-police = PoliceCar('Ford', 180, 'red')
-
-#вариант 2
+# вариант 1
 
 class Car:
+    _name = None
+    _speed = None
+    _color = None
+    _is_police = False
 
-    def __init__(self, speed, color, name, is_police):
-        try:
-            self.speed = speed
-            self.color = color
-            self.name = name
-            self.is_police = bool(is_police)
-            # if (not self.speed or len(self.speed) <= 0) or \
-            #         (not self.color or len(self.color) <= 0) or \
-            #         (not self.name or len(self.name) <= 0) or \
-            #         not bool(self.is_police):
-            #     raise BaseException
-        except BaseException:
-            return "Error. Please, enter data."
-            exit(-1)
+    def __init__(self, name, color, is_police=False):
+        self._name = name
+        self._speed = 0
+        self._color = color
+        self._is_police = is_police
 
-    def go(self):
-        return "Go."
+    def go(self, speed):
+        self._speed = speed
+        return "Машина поехала со скоростью " + str(speed)
 
     def stop(self):
-        return "Stop."
+        self._speed = 0
+        return "Машина остановилась"
 
-    def turn_left(self):
-        return "Turn left."
-
-    def turn_right(self):
-        return "Turn right."
+    def turn(self, direction):
+        return "машина повернула на" + direction
 
     def show_speed(self):
-        print("Machine normal speed.")
+        return "Скорость машины: " + str(self._speed)
 
 
 class TownCar(Car):
-    def __init__(self, speed, color, name, is_police):
-        super().__init__(speed, color, name, is_police)
+    #  Количество посадочных мест
+    __seats = None
+    __speed_limit = 60
+
+    def __init__(self, name, color, seats):
+        super().__init__(name, color)
+        self.__seats = seats
 
     def show_speed(self):
-        if self.speed > 40:
-            return "Over speed."
-        else:
-            return "Normal speed"
+        if self._speed > self.__speed_limit:
+            print(
+                f"Скорость {self._speed} превысила допустимый лимит! {self.__speed_limit}")
+        return super().show_speed()
 
 
 class SportCar(Car):
-    def __init__(self, speed, color, name, is_police):
-        super().__init__(speed, color, name, is_police)
+    def __init__(self, name, color):
+        super().__init__(name, color)
 
 
 class WorkCar(Car):
-    def __init__(self, speed, color, name, is_police):
-        super().__init__(speed, color, name, is_police)
+    __speed_limit = 40
+
+    def __init__(self, name, color, is_police):
+        super().__init__(name, color, is_police)
 
     def show_speed(self):
-        if self.speed > 60:
-            return "Over speed."
-        else:
-            return "Normal speed"
+        if self._speed > self.__speed_limit:
+            print(
+                f"Скорость {self._speed} превысила допустимый лимит! ({self.__speed_limit})")
+        return super().show_speed()
 
 
 class PoliceCar(Car):
-    def __init__(self, speed, color, name, is_police):
-        super().__init__(speed, color, name, is_police)
+    def __init__(self, name, color):
+        super().__init__(name, color, True)
 
 
-toyota = SportCar(100, 'Red', 'Toyota', False)
-mazda = TownCar(30, 'White', 'Mazda', True)
-suzuki = WorkCar(70, 'Rose', 'Suzuki', False)
-honda = PoliceCar(110, 'Blue',  'honda', True)
-
-
-print(toyota.turn_left())
-print(mazda.turn_right())
-print(suzuki.stop())
-print(honda.go())
-print(suzuki.is_police)
-print(honda.is_police)
-print(suzuki.show_speed())
-print(honda.show_speed())
-
-#вариант 3
-
-""" Показать текущуую скорость """
-        print(f'My speed is {self.speed} km/h')
-
-        if (hasattr(self, '_max_granted_speed')
-                and self._max_granted_speed
-                and self.speed > self._max_granted_speed):
-            print(f'Over speed on {self.speed - self._max_granted_speed} km/h')
-
-    @property
-    def direction(self):
-        """ Показать текущее направление """
-
-class TownCar(Car):
-    """ Класс городских автомобилей """
-
-    # максимальная скорость движения
-    _max_granted_speed = 60
-
-    def __init__(self, *args):
-        """
-        :param args: Параметры авто
-        """
-        super().__init__(*args)
-
-    def show_speed(self):
-        """ Показать скорость авто, и если
-        превышена так же сообщить об этом """
-        super().show_speed()
-        if self.speed > self._max_granted_speed:
-            print('Over speed')
-
-
-class SportCar(Car):
-    """ Класс спортивный автомобилей """
-    def __init__(self, *args):
-        """
-        :param args: Параметры авто
-        """
-        super().__init__(*args)
-    pass
-
-
-class WorkCar(Car):
-    """ Класс авто для работы """
-
-    # максимальная скорость движения
-    _max_granted_speed = 40
-
-    def __init__(self, *args):
-        """
-        :param args: Параметры авто
-        """
-        super().__init__(*args)
-
-    def show_speed(self):
-        """ Показать скорость авто, и если
-        превышена так же сообщить об этом """
-        super().show_speed()
-        if self.speed > self._max_granted_speed:
-            print('Over speed')
-
-
-class PoliceCar(Car):
-    """ Класс полицейского авто """
-    def __init__(self, *args):
-        """
-        :param args: Параметры авто
+honda = TownCar('Honda', 'blue', 5)
+print(honda._name, honda._color, honda._speed, honda._is_police)
+print(honda.go(60), honda.turn('лево'), honda.show_speed(), honda.stop(),
+      honda.show_speed())
+print(honda.go(100), honda.turn('лево'), honda.show_speed(), honda.stop(),
+      honda.show_speed())
+sportcar = SportCar('Ferrari', 'red')
+print(sportcar.go(120), sportcar.turn("право"), sportcar.show_speed(),
+      sportcar.stop())
+workcar1 = WorkCar('Lada', 'white', True)
+print(workcar1.go(100), workcar1.turn('лево'), workcar1.show_speed(),
+      workcar1.stop())
+print(workcar1.go(35), workcar1.turn('лево'), workcar1.show_speed(),
+      workcar1.stop())
+workcar2 = WorkCar('Hyundai', 'yellow', False)
+print(workcar2.go(100), workcar2.turn('право'), workcar2.show_speed(),
+      workcar2.stop())
+police = PoliceCar('Lada', 'black')
+print(police._name, police._color, police._speed, police._is_police)
+print(police.go(100), police.turn('лево'), police.show_speed(), police.stop())
+print(police._name, police._color, police._speed, police._is_police)
